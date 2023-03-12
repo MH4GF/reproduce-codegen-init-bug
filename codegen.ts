1b3e75ffbed6ca825d4e325576a77eac2c2de832
MH4GF <h.miyagi.cnw@gmail.com>
2023-03-12 16:06:42 +0900

    Comment out documents because we are not adding any operations

--- codegen.ts
+++ codegen.ts
@@ -4,7 +4,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   overwrite: true,
   schema: "https://swapi-graphql.netlify.app/.netlify/functions/index",
-  documents: "src/**/*.tsx",
+  // documents: "src/**/*.tsx",
   generates: {
     "src/gql": {
       preset: "client",
